bd314ef1851c9c33aa34e0618702518a2f130012
Eric Lam <eric.lam42@hotmail.com>
2025-06-17 01:28:29 -0400

    fix with the data

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div className="flex flex-col items-center w-full">
         <h1 className="text-6xl font-bold">UFClue</h1>
         <h2 className="text-xl">A UFC Fighter guessing game</h2>
-        <div className="text-xs">Updated as of 06-12-2025</div>
+        <div className="text-xs">Updated as of 06-15-2025</div>
         <div className="border border-[var(--borderColor)] mt-3 w-full"></div>
       </div>
       <ClientHome />
